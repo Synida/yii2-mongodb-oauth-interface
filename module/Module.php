c97bbc2637bc58ed4c9238a1be005aa1a7017330
Synida <synida@protonmail.com>
2022-11-01 00:05:36 +0300

    Extending module class

---
+++ module/Module.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by Synida Pry.
+ * Copyright © 2022. All rights reserved.
+ */
+
+namespace synida\module;
+
+use \filsh\yii2\oauth2server\Module as OAuthServerModule;
+use filsh\yii2\oauth2server\Server;
+use ReflectionException;
+use synida\response\AccessTokenResponseType;
+use synida\storage\Yii2MongoDB;
+use yii\base\InvalidConfigException;
+use yii\di\NotInstantiableException;
+
+/**
+ * Class Module
+ * @package synida\module
+ */
+class Module extends OAuthServerModule
+{
+    /**
+     * Gets Oauth2 Server
+     *
+     * @return Server
+     * @throws ReflectionException
+     * @throws InvalidConfigException
+     * @throws NotInstantiableException
+     */
+    public function getServer()
+    {
+        $server = parent::getServer();
+
+        /** @var Yii2MongoDB $accessTokenStorage */
+        $accessTokenStorage = $server->getStorage('access_token');
+
+        /** @var Yii2MongoDB $refreshTokenStorage */
+        $refreshTokenStorage = $server->getStorage('refresh_token');
+
+        $accessTokenResponseType = new AccessTokenResponseType($accessTokenStorage, $refreshTokenStorage, [
+            'use_jwt_access_tokens' => $this->useJwtToken,
+            'token_param_name' => $this->tokenParamName,
+            'access_lifetime' => $this->tokenAccessLifetime,
+        ]);
+        $server->addResponseType($accessTokenResponseType, 'token');
+
+        return $server;
+    }
+}
